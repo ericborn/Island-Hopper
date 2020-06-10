b57db0e69ca07af1f998023c8c752a0e2ef10734
Eric Born <eric.born85@gmail.com>
2020-06-10 15:27:43 -0700

    Split code into separate files

--- main.py
+++ main.py
@@ -5,217 +5,90 @@ Created on Wed Jun 10 11:15:24 2020
 @author: Eric
 """
 
-import pygame
+import pygame as pg
+import sys
+from os import path
+from constants import *
+from sprites import *
+from tilemap import *
 
-pygame.init()
-
-# constants
-FPS = 60
-
-# colors
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-WHITE = (255, 255, 255)
-WATER = (176, 233, 252)
-
-#bricks
-TILES_PER_ROW = 10
-NUM_ROWS = 5
-#BLANK_ROWS = 2
-
-# game stats
-score = 0
-lives = 100
-game_over = False
-
-# screen and clock setup
-screen = pygame.display.set_mode((1280, 960))
-screen_rect = screen.get_rect()
-clock = pygame.time.Clock()
-
-# character speed
-char_speed = 5
-boat_speed = 5
-
-# ball sprite class
-# sets size, color, starting position and speed
-class Ship(pygame.sprite.Sprite):
+class Game:
     def __init__(self):
-        super().__init__()
-        ship_image = pygame.image.load('assets\ship.png').convert_alpha()
-        self.image = pygame.transform.scale(ship_image, (50, 50))
-        self.rect = self.image.get_rect()
-        self.reset()
- 
-    def reset(self):
-        self.rect.center = screen_rect.center
-        self.x_speed = 0
-        self.y_speed = 0
-        self.died = False
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.load_data()
 
-    # prevents the ball from bouncing off the screen   
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        assets_folder = path.join(game_folder, 'assets')
+        self.map = Map(path.join(game_folder, 'map3.txt'))
+        self.player_img = pg.image.load(path.join(assets_folder, PLAYER_IMG)).convert_alpha()
+
+    def new(self):
+        # initialize all variables and do all the setup for a new game
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        for row, tiles in enumerate(self.map.data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col, row)
+                if tile == 'P':
+                    self.player = Player(self, col, row)
+        self.camera = Camera(self.map.width, self.map.height)
+
+    def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000.0
+            self.events()
+            self.update()
+            self.draw()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
     def update(self):
-        if self.rect.right >= screen_rect.right:
-            self.x_speed = -1
-        if self.rect.left <= screen_rect.left:
-            self.x_speed = 1
-        if self.rect.top <= screen_rect.top:
-            self.y_speed = 1
-        if self.rect.bottom >= screen_rect.bottom:
-            self.lost = True
+        # update portion of the game loop
+        self.all_sprites.update()
+        self.camera.update(self.player)
 
-    # updates speed
-        self.rect.x += self.x_speed
-        self.rect.y += self.y_speed
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
 
-# paddle class
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()      
-        player_image = pygame.image.load('assets\pirate.png').convert_alpha()
-        self.image = pygame.transform.scale(player_image, (30, 30))
-        self.rect = self.image.get_rect()
-        self.reset()
-    
-    def reset(self):
-        self.rect.center = screen_rect.center
-        self.x_speed = 0
-        self.y_speed = 0
-        self.died = False
-    
-    def update(self):
-        keys = pygame.key.get_pressed()
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, self.camera.apply(sprite))
+        # pg.draw.rect(self.screen, WHITE, self.player.hit_rect, 2)
+        pg.display.flip()
 
-        # allows paddle to be moved with left and right arrow keys
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += char_speed
-        if keys[pygame.K_LEFT]:
-            self.rect.x -= char_speed
-        if keys[pygame.K_UP]:
-            self.rect.y -= char_speed
-        if keys[pygame.K_DOWN]:
-            self.rect.y += char_speed
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
 
-        if keys[pygame.K_d]:
-            self.rect.x += char_speed
-        if keys[pygame.K_a]:
-            self.rect.x -= char_speed
-        if keys[pygame.K_w]:
-            self.rect.y -= char_speed
-        if keys[pygame.K_s]:
-            self.rect.y += char_speed
-            
-        # prevents paddle from leaving screen
-#        if self.rect.right >= screen_rect.right:
-#            self.rect.right = screen_rect.right
-#            
-#        if self.rect.left <= screen_rect.left:
-#            self.rect.left = screen_rect.left
-#            
-#        if self.rect.bottom <= screen_rect.bottom:
-#            self.rect.bottom = screen_rect.bottom   
-#            
-#        if self.rect.top >= screen_rect.top:
-#            self.rect.top = screen_rect.top
+    def show_start_screen(self):
+        pass
 
-class Sand(pygame.sprite.Sprite):
-    def __init__(self, row, col):
-        super().__init__()
-        sand_image = pygame.image.load('assets\sand.png').convert_alpha()
-        
-        # calculate new size based on TILES_PER_ROW
-        tile_width = round(screen_rect.width / TILES_PER_ROW)
-        orig_size = sand_image.get_rect()
-        scale_factor = (tile_width / orig_size.width)
-        tile_height = round(orig_size.height * scale_factor)
-        new_size = (tile_width, tile_height)
- 
-        
-        # scale the image
-        self.image = pygame.transform.scale(sand_image, new_size)
-        self.rect = self.image.get_rect()
-        
-        # position the bricks
-#        row += BLANK_ROWS
-        self.rect.x = col * tile_width
-        self.rect.y = row * tile_height
+    def show_go_screen(self):
+        pass
 
-# creates a group for all sprites
-all_sprites = pygame.sprite.Group()
-#sand_tiles = pygame.sprite.Group()
-
-# instantiate the brick class, add brick to sprite group
-for row in range(0, NUM_ROWS):
-    for col in range(0, TILES_PER_ROW):
-        brick = Sand(row,col)
-        all_sprites.add(brick)
-        #sand_tiles.add(brick)
-
-# instantiate the ship class, add ship to sprite group
-ship = Ship()
-all_sprites.add(ship)
-
-# instantiate the player class, add player to sprite group
-player = Player()
-all_sprites.add(player)
-
-
-        
-def draw_text(surface, text, pos=(0,0), color=WHITE, font_size=20, anchor='topleft'):
-    arial = pygame.font.match_font('arial')
-    font = pygame.font.Font(arial, font_size)
-    text_surface = font.render(text, True, color)
-    text_rect = text_surface.get_rect()
-    setattr(text_rect, anchor, pos)
-    surface.blit(text_surface, text_rect)
-
-# while loop that runs the game
-running = True
-while running:
-    # sets the clock tick rate to our FPS constant
-    clock.tick(FPS)
-
-    # allows the game to be closed and quit the program on clicking the X
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    if game_over:
-        screen.fill(BLACK)
-        draw_text(screen, "GAME OVER", screen_rect.center, font_size=80, anchor="center")
-    
-    # updates the sprites group on each tick of the game
-    else:
-        all_sprites.update()    
-    
-        
-        # reset ball if it passes below the bottom of screen
-        if ship.died:
-            lives -= 1
-            if lives == 0:
-                game_over = True
-            ship.reset()
-        
-        # check for ball hitting bricks, bounce down if it did
-#        collide_brick = pygame.sprite.spritecollideany(ship, bricks)
-#        if collide_brick:
-#            score += 1
-#            collide_brick.kill()
-#            ship.y_speed *= -1
-        
-        # fills the background with black
-        screen.fill(WATER)
-        
-        # draws the sprites on screen
-        all_sprites.draw(screen)
-        
-        score_text = f"Score: {score} / Lives: {lives}"
-        draw_text(screen, score_text, (8, 8))
-        
-    #flips the display
-#    pygame.display.flip()
-    
-    pygame.display.update() 
-
-pygame.quit()
+# create the game object
+g = Game()
+g.show_start_screen()
+while True:
+    g.new()
+    g.run()
+    g.show_go_screen()
---
+++ sprites.py
@@ -0,0 +1,70 @@
+import pygame as pg
+from constants import *
+from tilemap import collide_hit_rect
+vec = pg.math.Vector2
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.player_img
+        self.rect = self.image.get_rect()
+        self.hit_rect = PLAYER_HIT_RECT
+        self.hit_rect.center = self.rect.center
+        self.vel = vec(0, 0)
+        self.pos = vec(x, y) * TILESIZE
+        self.rot = 0
+
+    def get_keys(self):
+        self.vel = vec(0, 0)
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.vel.x = -PLAYER_SPEED
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.vel.x = PLAYER_SPEED
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.vel.y = -PLAYER_SPEED
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.vel.y = PLAYER_SPEED
+
+    def collide_with_walls(self, dir):
+        if dir == 'x':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False, collide_hit_rect)
+            if hits:
+                if self.vel.x > 0:
+                    self.pos.x = hits[0].rect.left - self.hit_rect.width / 2.0
+                if self.vel.x < 0:
+                    self.pos.x = hits[0].rect.right + self.hit_rect.width / 2.0
+                self.vel.x = 0
+                self.hit_rect.centerx = self.pos.x
+        if dir == 'y':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False, collide_hit_rect)
+            if hits:
+                if self.vel.y > 0:
+                    self.pos.y = hits[0].rect.top - self.hit_rect.height / 2.0
+                if self.vel.y < 0:
+                    self.pos.y = hits[0].rect.bottom + self.hit_rect.height / 2.0
+                self.vel.y = 0
+                self.hit_rect.centery = self.pos.y
+
+    def update(self):
+        self.get_keys()
+        self.pos += self.vel * self.game.dt
+        self.rect.x = self.pos.x
+        self.collide_with_walls('x')
+        self.rect.y = self.pos.y
+        self.collide_with_walls('y')
+
+class Wall(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
